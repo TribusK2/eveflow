eccae2dd5b0e0c49ed9db2a865fa5fe248d0aa41
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2020-07-25 09:05:44 +0200

    Dial clock simple view added

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
+
+declare interface Marker {
+  rotate: number;
+  posX: number;
+  posY: number;
+}
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.scss']
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('clockDial', { static: true }) clockDial: ElementRef;
+
+  public markers: Marker[] = [];
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+
+    const clockHeight = parseInt(window.getComputedStyle(this.clockDial.nativeElement).getPropertyValue('height'));
+
+    for (let i = 0; i < 12; i++) {
+      let marker: Marker = {
+        rotate: i * 30,
+        posX: (clockHeight/2) * Math.sin(i * 30 * (Math.PI / 180)),
+        posY: (-clockHeight/2) * Math.cos(i * 30 * (Math.PI / 180)) + clockHeight/2
+      };
+      this.markers.push(marker);
+    }
+
+  }
+
+  ngAfterViewInit(){
+    
   }
 
 }
